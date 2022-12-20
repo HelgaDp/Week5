f86f0b1bcfd2eb0fe065790c036a722577854ee3
HelgaDp <106965471+HelgaDp@users.noreply.github.com>
2022-12-20 15:18:22 +0200

    Add files via upload

---
+++ WeatherForecast.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from "react";
+import WeatherForecastDay from "./WeatherForecastDay";
+import "./WeatherForecast.css";
+import axios from "axios";
+
+export default function WeatherForecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  function handleResponse(response) {
+    setForecast(response.data.daily);
+    setLoaded(true);
+  }
+
+  if (loaded) {
+    return (
+      <div className="cityNextTempContainer" id="forecast">
+        <div className="Row">
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 5) {
+              return (
+                <div class="daysCol Col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            } else {
+              return null;
+            }
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    let apiKey = "6ffb8b485c9072dba8939b44a23fbecc";
+    let longitude = props.coordinates.lon;
+    let latitude = props.coordinates.lat;
+    let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=metric`;
+
+    axios.get(apiUrl).then(handleResponse);
+    return null;
+  }
+}
